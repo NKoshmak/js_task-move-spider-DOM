b864719a3d59dc310cf7bb1c1220e50d64741b90
Koshmak Nataliia <koshmaknatali@gmail.com>
2024-08-17 19:45:40 -0600

    make build

---
+++ index.42270071.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(t){var e=document.querySelector(".wall"),l=document.querySelector(".spider"),i=e.getBoundingClientRect();if(t.target.closest(".wall")){var c={top:t.clientY-i.top-e.clientTop-l.clientHeight/2,left:t.clientX-i.left-e.clientLeft-l.clientWidth/2};c.top<0&&(c.top=0),c.left<0&&(c.left=0),c.left+l.clientWidth>e.clientWidth&&(c.left=e.clientWidth-l.clientWidth),c.top+l.clientHeight>e.clientHeight&&(c.top=e.clientHeight-l.clientHeight),l.style.left=c.left+"px",l.style.top=c.top+"px"}});
+//# sourceMappingURL=index.42270071.js.map
